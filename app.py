25cac84289cca63c13098351b3ec8dff0e936b42
VanWagonerLauren <77031397+VanWagonerLauren@users.noreply.github.com>
2021-03-29 21:15:29 -0500

    Dictionary

--- app.py
+++ app.py
@@ -41,3 +41,16 @@ results = session.query(Measurement.date, Measurement.prcp).\
 
 session.close()
 
+#Dictionary
+  all_prcp = []
+    for date,prcp  in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+               
+        all_prcp.append(prcp_dict)
+
+    return jsonify(all_prcp)
+    
+
+
