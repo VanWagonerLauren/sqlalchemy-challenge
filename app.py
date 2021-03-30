14d43b46c85218a0a7194dadbf9b3f7c6c893ed4
VanWagonerLauren <77031397+VanWagonerLauren@users.noreply.github.com>
2021-03-29 21:09:04 -0500

    Flask

--- app.py
+++ app.py
@@ -10,3 +10,22 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
+#Flask
+app = Flask(__name__)
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Welcome to the SQL-Alchemy APP API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
