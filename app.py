86b68752b5eeb682c81c7c4da33b34ec367fbc27
VanWagonerLauren <77031397+VanWagonerLauren@users.noreply.github.com>
2021-03-29 21:03:56 -0500

    Database and Engine

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+
+#Database and Engine
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
